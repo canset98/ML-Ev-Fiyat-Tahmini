2fd12916535b6e8e106e40f6ac329c63e7ca6309
Tuba-1234 <032290045@ogr.uludag.edu.tr>
2024-05-23 02:05:37 +0300

    ......son aşama bitti

--- Ara_YuzOlusturma/Ara_YuzOlusturma.py
+++ Ara_YuzOlusturma/Ara_YuzOlusturma.py
@@ -17,11 +17,7 @@ df_2.drop("Unnamed: 0", axis=1, inplace=True)
 df = df_2.copy()
 
 # Fiyat tahmininde bize evin değerinin pahalı yada ucuz olduğunu hesapta göstermeye yarayan sütunlarımızı giriyoruz...
-df = df[['İlçe', 'Türü', 'Net_Metrekare', 'Brüt_Metrekare', 'Oda_Sayısı',
-                     'Binanın_Yaşı', 'Isıtma_Tipi', 'Site_İçerisinde', 'Kategorisi',
-                     'Eşya_Durumu', 'Takas', 'Bulunduğu_Kat', 'Banyo_Sayısı',
-                     'Binanın_Kat_Sayısı', 'Fiyat_Durumu','Fiyatı', 'yaka', 'İl', 'Kullanım_Durumu',
-                     'Nüfus', 'Yaşam_endeksi', 'Mahalle', 'Krediye_Uygunluk', 'Yatırıma_Uygunluk']]
+df = df[['İlçe', 'Krediye_Uygunluk', 'Net_Metrekare', 'Brüt_Metrekare', 'Oda_Sayısı', 'Binanın_Yaşı', 'Isıtma_Tipi', 'Site_İçerisinde', 'Eşya_Durumu', 'Banyo_Sayısı', 'Yatırıma_Uygunluk', 'Bulunduğu_Kat','Fiyatı']]
 
 X = df.drop(["Fiyatı"], axis=1)
 y = df["Fiyatı"]
@@ -52,42 +48,47 @@ def olumsuz():
 def İlçe_düzenle():
     global İlçe
     İlçe_deger = İlçe_kutu.get()
-    if İlçe_deger == "Sarıyer":
+    if İlçe_deger == "Avcılar":
         İlçe = 0
         mesaj()
-    elif İlçe_deger == "Silivri":
+    elif İlçe_deger == "Bahçelievler":
         İlçe = 1
         mesaj()
-    elif İlçe_deger == "Sultanbeyli":
+    elif İlçe_deger == "Bakırköy":
         İlçe = 2
         mesaj()
-    elif İlçe_deger == "Sultangazi":
+    elif İlçe_deger == "Bağcılar":
         İlçe = 3
         mesaj()
-    elif İlçe_deger == "Tuzla":
+    elif İlçe_deger == "Başakşehir":
         İlçe = 4
         mesaj()
-    elif İlçe_deger == "Zeytinburnu":
+    elif İlçe_deger == "Beylikdüzü":
         İlçe = 5
         mesaj()
-    elif İlçe_deger == "Çekmeköy":
+    elif İlçe_deger == "Beşiktaş":
         İlçe = 6
         mesaj()
-    elif İlçe_deger == "Ümraniye":
+    elif İlçe_deger == "Esenyurt":
         İlçe = 7
         mesaj()
-    elif İlçe_deger == "Sultanbeyli":
+    elif İlçe_deger == "Kadıköy":
         İlçe = 8
         mesaj()
-    elif İlçe_deger == "Üsküdar":
+    elif İlçe_deger == "Maltepe":
         İlçe = 9
         mesaj()
-    elif İlçe_deger == "Şile":
+    elif İlçe_deger == "Pendik":
         İlçe = 10
         mesaj()
-    elif İlçe_deger == "Şişli":
+    elif İlçe_deger == "Sarıyer":
         İlçe = 11
         mesaj()
+
+    elif İlçe_deger == "Şişli":
+        İlçe = 12
+        mesaj()
+
     else:
         olumsuz()
     print(İlçe)
@@ -96,12 +97,17 @@ def İlçe_düzenle():
 def Kredi_düzenle():
     global Kredi
     kredi_deger = kredi_kutu.get()  # == yerine = kullanarak değeri atıyoruz
-    if kredi_deger == "Krediye Uygun":
+    if kredi_deger == "Bilinmiyor":
         Kredi = 0
         mesaj()
-    elif kredi_deger == "Krediye Uygun Değil":
+    elif kredi_deger == "Krediye Uygun":
         Kredi = 1
         mesaj()
+
+    elif kredi_deger == "Krediye Uygun Değil":
+        Kredi = 2
+        mesaj()
+
     else:
         olumsuz()
     print(Kredi)
@@ -123,63 +129,81 @@ def net_düzenle():
 def oda_düzenle():
     global oda
     oda_deger = oda_kutu.get()
-    if oda_deger == "1":
+    if oda_deger == "1 Oda":
         oda = 0
         mesaj()
     elif oda_deger == "1+1":
         oda = 1
         mesaj()
-    elif oda_deger == "2+1":
+    elif oda_deger == "1.5+1":
         oda = 2
         mesaj()
-    elif oda_deger == "2+2":
+    elif oda_deger == "2+0":
         oda = 3
         mesaj()
-    elif oda_deger == "3+1":
+    elif oda_deger == "2+1":
         oda = 4
         mesaj()
-    elif oda_deger == "3+2":
+    elif oda_deger == "2+2":
         oda = 5
         mesaj()
-    elif oda_deger == "4+1":
+    elif oda_deger == "2.5+1":
         oda = 6
         mesaj()
-    elif oda_deger == "4+2":
+    elif oda_deger == "3+1":
         oda = 7
         mesaj()
-    elif oda_deger == "5+1":
+    elif oda_deger == "3+2":
         oda = 8
         mesaj()
-    elif oda_deger == "5+2":
+    elif oda_deger == "3.5+1":
         oda = 9
         mesaj()
-    elif oda_deger == "5+3":
+    elif oda_deger == "4+1":
         oda = 10
         mesaj()
-    elif oda_deger == "6+1":
+    elif oda_deger == "4+2":
         oda = 11
         mesaj()
-    elif oda_deger == "6+2":
+    elif oda_deger == "4.5+1":
         oda = 12
         mesaj()
-    elif oda_deger == "6+3":
+    elif oda_deger == "5+1":
         oda = 13
         mesaj()
-    elif oda_deger == "7+2":
+    elif oda_deger == "5+2":
         oda = 14
         mesaj()
-    elif oda_deger == "7+3":
+    elif oda_deger == "5+3":
         oda = 15
         mesaj()
-    elif oda_deger == "8+":
+    elif oda_deger == "6+1":
         oda = 16
         mesaj()
-    elif oda_deger == "Stüdyo":
+    elif oda_deger == "6+2":
         oda = 17
+        mesaj()
+    elif oda_deger == "6+3":
+        oda = 18
+        mesaj()
+    elif oda_deger == "6+4":
+        oda = 19
+        mesaj()
+    elif oda_deger == "7+1":
+        oda = 20
+        mesaj()
+    elif oda_deger == "7+2":
+        oda = 21
+        mesaj()
+    elif oda_deger == "8+Oda":
+        oda = 22
+        mesaj()
+    elif oda_deger == "Stüdyo":
+        oda = 23
         mesaj()
     else:
         olumsuz()
-
+    print(oda)
 
 def yaş_düzenle():
     global yaş
@@ -190,27 +214,30 @@ def yaş_düzenle():
     elif yaş_deger == "1":
         yaş = 1
         mesaj()
-    elif yaş_deger == "2":
-        yaş = 3
-        mesaj()
-    elif yaş_deger == "3":
-        yaş = 5
-        mesaj()
-    elif yaş_deger == "4":
-        yaş = 6
-        mesaj()
-    elif yaş_deger == "5-10":
-        yaş = 7
-        mesaj()
-    elif yaş_deger == "11-20":
+    elif yaş_deger == "11-15":
         yaş = 2
         mesaj()
-    elif yaş_deger == "20 ve üzeri":
+    elif yaş_deger == "16-20":
+        yaş = 3
+        mesaj()
+    elif yaş_deger == "2":
         yaş = 4
+        mesaj()
+    elif yaş_deger == "21 Ve Üzeri":
+        yaş = 5
+        mesaj()
+    elif yaş_deger == "3":
+        yaş = 6
+        mesaj()
+    elif yaş_deger == "4":
+        yaş = 7
+        mesaj()
+    elif yaş_deger == "5-10":
+        yaş = 8
         mesaj()
     else:
         olumsuz()
-
+    print(yaş)
 
 def site_düzenle():
     global site
@@ -223,20 +250,20 @@ def site_düzenle():
         mesaj()
     else:
         olumsuz()
-
+    print(site)
 
 def eşya_düzenle():
     global eşya
     eşya_deger = eşya_kutu.get()
-    if eşya_deger == "Boş":
+    if eşya_deger == "Var":
         eşya = 0
         mesaj()
-    elif eşya_deger == "Eşyalı":
+    elif eşya_deger == "Yok":
         eşya = 1
         mesaj()
     else:
         olumsuz()
-
+    print(eşya)
 
 def alan_düzenle():
     global alan
@@ -252,39 +279,24 @@ def alan_düzenle():
 def ısıtma_düzenle():
     global ısıtma
     ısıtma_deger = ısıtma_kutu.get()
-    if ısıtma_deger == "Doğalgaz Sobalı":
+    if ısıtma_deger == "Doğalgaz":
         ısıtma = 0
         mesaj()
-    elif ısıtma_deger == "Güneş Enerjisi":
+    elif ısıtma_deger == "Isıtma Yok":
         ısıtma = 1
         mesaj()
-    elif ısıtma_deger == "Isıtma Yok":
+    elif ısıtma_deger == "Klimalı":
         ısıtma = 2
         mesaj()
-    elif ısıtma_deger == "Kat Kaloriferi":
+    elif ısıtma_deger == "Sobalı":
         ısıtma = 3
         mesaj()
-    elif ısıtma_deger == "Klimalı":
-        ısıtma = 4
-        mesaj()
-    elif ısıtma_deger == "Kombi Doğalgaz":
-        ısıtma = 5
-        mesaj()
-    elif ısıtma_deger == "Merkezi (Pay Ölçer)":
-        ısıtma = 6
-        mesaj()
-    elif ısıtma_deger == "Merkezi Doğalgaz":
-        ısıtma = 7
-        mesaj()
-    elif ısıtma_deger == "Sobalı":
-        ısıtma = 8
-        mesaj()
     elif ısıtma_deger == "Yerden Isıtma":
-        ısıtma = 9
+        ısıtma = 4
         mesaj()
     else:
         olumsuz()
-
+    print(ısıtma)
 
 def banyo_düzenle():
     global banyo
@@ -295,33 +307,39 @@ def banyo_düzenle():
     elif banyo_deger == "1":
         banyo = 1
         mesaj()
-    elif banyo_deger == "2":
+    elif banyo_deger == "15":
         banyo = 2
         mesaj()
-    elif banyo_deger == "3":
+    elif banyo_deger == "2":
         banyo = 3
         mesaj()
-    elif banyo_deger == "4":
+    elif banyo_deger == "3":
         banyo = 4
         mesaj()
-    elif banyo_deger == "5":
+    elif banyo_deger == "4":
         banyo = 5
         mesaj()
-    elif banyo_deger == "6+":
+    elif banyo_deger == "5":
         banyo = 6
+        mesaj()
+    elif banyo_deger == "6":
+        banyo = 7
         mesaj()
     else:
         olumsuz()
-
+    print(banyo)
  #Yatırım
 def Yatırım_düzenle():
     global Yatırım
     yatırım_deger = yatırım_kutu.get()  # == yerine = kullanarak değeri atıyoruz
-    if yatırım_deger == "Yatırıma Uygun":
+    if yatırım_deger == "Bilinmiyor":
         Yatırım  = 0
         mesaj()
-    elif yatırım_deger == "Yatırıma Uygun Değil":
+    elif yatırım_deger == "Yatırıma Uygun":
         Yatırım = 1
+        mesaj()
+    elif yatırım_deger == "Yatırıma Uygun Değil":
+        Yatırım = 2
         mesaj()
     else:
         olumsuz()
@@ -349,45 +367,45 @@ def daire_düzenle():
     elif daire_deger == "2":
         daire = 5
         mesaj()
-    elif daire_deger == "20-30":
+    elif daire_deger == "21-29":
         daire = 6
         mesaj()
     elif daire_deger == "3":
-        daire = 8
+        daire = 7
         mesaj()
     elif daire_deger == "30-40":
-        daire = 9
+        daire = 8
         mesaj()
     elif daire_deger == "4":
-        daire = 10
+        daire = 9
         mesaj()
     elif daire_deger == "5":
-        daire = 11
+        daire = 10
         mesaj()
     elif daire_deger == "6":
-        daire = 12
+        daire = 11
         mesaj()
     elif daire_deger == "7":
-        daire = 13
+        daire = 12
         mesaj()
     elif daire_deger == "8":
-        daire = 14
+        daire = 13
         mesaj()
     elif daire_deger == "9":
-        daire = 15
+        daire = 14
         mesaj()
     elif daire_deger == "Bodrum":
-        daire = 16
+        daire = 15
         mesaj()
     elif daire_deger == "Giriş":
-        daire = 17
+        daire = 16
         mesaj()
     elif daire_deger == "Çatı Katı":
-        daire = 18
+        daire = 17
         mesaj()
     else:
         olumsuz()
-
+    print(daire)
 
 baslık_label = Label(pencere, text="EV FİYAT TAHMİNİ", font="helvetica 50", borderwidth=20, padx=350, pady=40, background="#7B2049")
 baslık_label.place(x=70, y=20)
@@ -396,7 +414,7 @@ baslık_label.place(x=70, y=20)
 ilce_label = Label(text="İlçe Seçimi", font="helvetica 12", borderwidth=6,background='#418EC6')
 ilce_label.place(x=100, y=250)
 
-İlçeler = ["Çekmeköy", "Sarıyer", "Silivri", "Sultanbeyli", "Sultangazi", "Şile", "Şişli", "Tuzla", "Ümraniye", "Üsküdar", "Zeytinburnu"]
+İlçeler = ["Avcılar", "Bahçelievler", "Bakırköy", "Bağcılar", "Başakşehir", "Beylikdüzü", "Beşiktaş", "Esenyurt", "Kadıköy", "Maltepe", "Pendik","Sarıyer","Şişli"]
 İlçe_kutu = Combobox(pencere, values=İlçeler)
 İlçe_kutu.place(x=100, y=300)
 
@@ -407,7 +425,7 @@ ilce_buton.place(x=100, y=350)
 kredi_label=Label(text="Krediye Uygunluk", font="helvetica 12", borderwidth=6,background='#418EC6')
 kredi_label.place(x=300, y=250)
 
-uygunluk = ["Krediye Uygun","Krediye Uygun Değil"]
+uygunluk = ["Bilinmiyor","Krediye Uygun","Krediye Uygun Değil"]
 kredi_kutu = Combobox(pencere, values=uygunluk)
 kredi_kutu.place(x=300,y=300)
 
@@ -420,7 +438,7 @@ kredi_buton.place(x=300, y=350)
 oda_label = Label(text="Oda Sayısı Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 oda_label.place(x=500, y=250)
 
-odalar = ["1", "1+1", "2+1", "2+2", "3+1", "3+2", "4+1", "4+2", "5+1", "5+2", "5+3", "6+1", "6+2", "6+3", "7+2", "7+3", "8+", "Stüdyo"]
+odalar = ["1 Oda", "1+1", "1.5+1", "2+0", "2+1", "2+2", "2.5+1", "3+1", "3+2", "3.5+1", "4+1", "4+2", "4.5+1", "5+1", "5+2", "5+3", "6+1","6+2","6+3","6+4","7+1","7+2","8+Oda","Stüdyo"]
 oda_kutu = Combobox(pencere, values=odalar)
 oda_kutu.place(x=500, y=300)
 
@@ -441,7 +459,7 @@ site_buton.place(x=700, y=350)
 eşya_label = Label(pencere, text="Eşya Durumunu Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 eşya_label.place(x=100, y=450)
 
-eşyalar = ["Boş", "Eşyalı"]
+eşyalar = ["Var", "Yok"]
 eşya_kutu = Combobox(pencere, values=eşyalar)
 eşya_kutu.place(x=100, y=500)
 
@@ -452,7 +470,7 @@ eşya_buton.place(x=100, y=550)
 yaş_label = Label(text="Bina Yaşını Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 yaş_label.place(x=300, y=450)
 
-yaşlar = ["0", "1", "2", "3", "4", "5-10", "11-20", "20 ve üzeri"]
+yaşlar = ["0", "1", "11-15", "16-20", "2", "21 Ve Üzeri", "3", "4","5-10"]
 yaşlar_kutu = Combobox(pencere, values=yaşlar)
 yaşlar_kutu.place(x=300, y=500)
 
@@ -483,7 +501,7 @@ alan_buton.place(x=700, y=550)
 ısıtma_label = Label(text="Isıtma Türünü Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 ısıtma_label.place(x=100, y=650)
 
-ısıtmalar = ["Doğalgaz Sobalı", "Güneş Enerjisi", "Isıtma Yok", "Kat Kaloriferi", "Klimalı", "Kombi Doğalgaz", "Merkezi (Pay Ölçer)", "Merkezi Doğalgaz", "Sobalı", "Yerden Isıtma"]
+ısıtmalar = ["Doğalgaz", "Isıtma Yok", "Klimalı", "Sobalı", "Yerden Isıtma"]
 ısıtma_kutu = Combobox(pencere, values=ısıtmalar)
 ısıtma_kutu.place(x=100, y=700)
 
@@ -494,7 +512,7 @@ alan_buton.place(x=700, y=550)
 banyo_label = Label(text="Banyo Türünü Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 banyo_label.place(x=300, y=650)
 
-banyolar = ["0", "1", "2", "3", "4", "5", "6+"]
+banyolar = ["0", "1", "15", "2", "3", "4", "5","6"]
 banyo_kutu = Combobox(pencere, values=banyolar)
 banyo_kutu.place(x=300, y=700)
 
@@ -505,7 +523,7 @@ banyo_buton.place(x=300, y=750)
 yatırım_label=Label(text="Yatırıma Uygunluk", font="helvetica 12", borderwidth=6,background='#418EC6')
 yatırım_label.place(x=500, y=650)
 
-uygunluk = ["Yatırıma Uygun","Yatırıma Uygun Değil"]
+uygunluk = ["Bilinmiyor","Yatırıma Uygun","Yatırıma Uygun Değil"]
 yatırım_kutu = Combobox(pencere, values=uygunluk)
 yatırım_kutu.place(x=500,y=700)
 
@@ -516,7 +534,7 @@ yatırım_buton.place(x=500, y=750)
 daire_label = Label(text="Daire Katını Seçiniz", font="helvetica 12", borderwidth=6,background='#418EC6')
 daire_label.place(x=700, y=650)
 
-daireler = ["-1", "-2", "-3", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10-20", "20-30", "30-40", "Bodrum", "Çatı Katı", "Giriş"]
+daireler = ["-1", "-2", "-3", "1", "10-20", "2", "21-29", "3", "30-40", "4", "5", "6", "7", "8", "9", "Bodrum", "Giriş", "Çatı Katı"]
 daire_kutu = Combobox(pencere, values=daireler)
 daire_kutu.place(x=700, y=700)
 
@@ -548,15 +566,14 @@ def hesapla():
     if pred < 0:
         pred = -1 * pred
     pred = int(pred)
-    s2 = Label(pencere, text=f"Tahmini Fiyat: {pred} TL", font="helvetica 20", borderwidth=6, padx=200, pady=40,background='#418EC6')
-    s2.place(x=1210, y=700)
+    s2 = Label(pencere, text=f"Tahmini Fiyat: {pred} TL", font="helvetica 20", borderwidth=6, padx=40, pady=35,background='#418EC6')
+    s2.place(x=1100, y=600)
 
 # HESAPLA
 hesapla_buton = Button(pencere, text="HESAPLA", command=hesapla, font="helvetica 15", borderwidth=60, padx=65, pady=40,background='#418EC6')
 hesapla_buton.place(x=1200, y=300)
 
-s1 = Label(pencere, text="", font="helvetica 12", borderwidth=6, padx=100, pady=35,background='#418EC6')
-s1.place(x=1250, y=600)
+
 
 # Pencere döngüsünü başlat
 pencere.mainloop()
